e07d5dd4b3ada650e8edf1d8ffe054df7fbf17f9
Nito Aldif <nitoaldif@gmail.com>
2023-12-21 08:53:46 +0700

    portfolio

---
+++ app/Http/Controllers/Admin/TyperTitleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\TyperTitle;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\DataTables\TyperTitleDataTable;
+
+class TyperTitleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(TyperTitleDataTable $datatable)
+    {
+        return $datatable->render('admin.typer-title.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.typer-title.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required', 'max:200']
+        ]);
+
+        $create = new TyperTitle();
+        $create->title = $request->title;
+        $create->save();
+
+        toastr()->success('Created successfully!', 'Congrats');
+        return redirect()->route('admin.typer-title.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HeroController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\TyperTitleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/blog', function () {
+    return view('frontend.blog');
+});
+
+Route::get('/dashboard',[DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+// Admin Router
+Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function() {
+
+    //Hero
+    Route::resource('hero', HeroController::class);
+    Route::resource('typer-title', TyperTitleController::class);
+});
