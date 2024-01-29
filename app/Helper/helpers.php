0f64b1959d84fe2390f5ebe031b58f639be3148b
Nito Aldif <nitoaldif@gmail.com>
2024-01-29 10:12:52 +0700

    Handle deleteFileIfExist

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -25,3 +25,16 @@ function handleUpload($inputName, $model=null) {
     }
   
 }    
+
+// Delete file
+
+function deleteFileIfExist($filePath){
+   
+    try{
+        if(\File::exists(public_path($filePath))){
+            \File::delete(public_path($filePath));
+        }
+    }catch(\Exception $e) {
+        throw $e;
+    }
+}
